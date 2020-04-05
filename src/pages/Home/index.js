5a16dc07a4978e2553d6052b6df60f5c28a47000
Luis Ruiz <luis20164@hotmail.es>
2020-04-05 08:53:45 -0500

    Initial template and home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import {
+  Container,
+  Typography,
+  Card,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
+
+import styles from "./style";
+
+export default () => {
+  const [searchText, setSearchText] = useState("");
+  const classes = styles();
+
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleCleanTextClick = () => {
+    console.log(`Hey`);
+  };
+  const handleSearchTextClick = () => {
+    console.log(`Hey`);
+  };
+
+  return (
+    <Container className={classes.container}>
+      <Card className={classes.cardContainer}>
+        <Grid container className={classes.titleGridContainer}>
+          <Grid>
+            <Typography className={classes.title}> Bienvenido!</Typography>
+          </Grid>
+          <Grid>
+            <label>Icono</label>
+          </Grid>
+        </Grid>
+        <TextField
+          value={searchText}
+          placeholder="Buscar..."
+          className={classes.textFieldSearch}
+          onChange={handleSearchTextChange}
+        />
+        <Grid className={classes.buttonsContainer}>
+          <Button variant="contained" onClick={handleCleanTextClick}>
+            Limpiar
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            className={classes.searchButton}
+            onClick={handleSearchTextClick}
+          >
+            Limpiar
+          </Button>
+        </Grid>
+      </Card>
+    </Container>
+  );
+};
---
+++ src/pages/Home/style.js
@@ -0,0 +1,37 @@
+import { makeStyles } from "@material-ui/styles";
+
+const centeredStyleObj = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+export default makeStyles({
+  container: {
+    height: "100vh",
+    flexDirection: "column",
+    ...centeredStyleObj,
+  },
+  cardContainer: {
+    ...centeredStyleObj,
+    width: 400,
+    height: 200,
+    padding: "2rem",
+    flexDirection: "column",
+  },
+  title: {
+    fontSize: "4rem",
+  },
+  tittleGridContainer: {
+    ...centeredStyleObj,
+  },
+  textFieldSearch: {
+    width: "90%",
+  },
+  searchButton: {
+    marginLeft: ".5rem",
+  },
+  buttonsContainer: {
+    marginTop: ".5rem",
+  },
+});
